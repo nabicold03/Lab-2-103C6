dd1d3b9f89386c13ae0397d78b198b059d493a28
nabicold03 <binh.vunabi2509@hcmut.edu.vn>
2023-09-27 16:43:06 +0700

    no message

--- Source Code/Core/Inc/main.h
+++ Source Code/Core/Inc/main.h
@@ -58,8 +58,64 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define ENM0_Pin GPIO_PIN_2
+#define ENM0_GPIO_Port GPIOA
+#define ENM1_Pin GPIO_PIN_3
+#define ENM1_GPIO_Port GPIOA
+#define DOT_Pin GPIO_PIN_4
+#define DOT_GPIO_Port GPIOA
 #define LED_RED_Pin GPIO_PIN_5
 #define LED_RED_GPIO_Port GPIOA
+#define On_1_Pin GPIO_PIN_6
+#define On_1_GPIO_Port GPIOA
+#define On_2_Pin GPIO_PIN_7
+#define On_2_GPIO_Port GPIOA
+#define Pin0_Pin GPIO_PIN_0
+#define Pin0_GPIO_Port GPIOB
+#define Pin1_Pin GPIO_PIN_1
+#define Pin1_GPIO_Port GPIOB
+#define Pin2_Pin GPIO_PIN_2
+#define Pin2_GPIO_Port GPIOB
+#define ROW2_Pin GPIO_PIN_10
+#define ROW2_GPIO_Port GPIOB
+#define ROW3_Pin GPIO_PIN_11
+#define ROW3_GPIO_Port GPIOB
+#define ROW4_Pin GPIO_PIN_12
+#define ROW4_GPIO_Port GPIOB
+#define ROW5_Pin GPIO_PIN_13
+#define ROW5_GPIO_Port GPIOB
+#define ROW6_Pin GPIO_PIN_14
+#define ROW6_GPIO_Port GPIOB
+#define ROW7_Pin GPIO_PIN_15
+#define ROW7_GPIO_Port GPIOB
+#define On_3_Pin GPIO_PIN_8
+#define On_3_GPIO_Port GPIOA
+#define On_4_Pin GPIO_PIN_9
+#define On_4_GPIO_Port GPIOA
+#define ENM2_Pin GPIO_PIN_10
+#define ENM2_GPIO_Port GPIOA
+#define ENM3_Pin GPIO_PIN_11
+#define ENM3_GPIO_Port GPIOA
+#define ENM4_Pin GPIO_PIN_12
+#define ENM4_GPIO_Port GPIOA
+#define ENM5_Pin GPIO_PIN_13
+#define ENM5_GPIO_Port GPIOA
+#define ENM6_Pin GPIO_PIN_14
+#define ENM6_GPIO_Port GPIOA
+#define ENM7_Pin GPIO_PIN_15
+#define ENM7_GPIO_Port GPIOA
+#define Pin3_Pin GPIO_PIN_3
+#define Pin3_GPIO_Port GPIOB
+#define Pin4_Pin GPIO_PIN_4
+#define Pin4_GPIO_Port GPIOB
+#define Pin5_Pin GPIO_PIN_5
+#define Pin5_GPIO_Port GPIOB
+#define Pin6_Pin GPIO_PIN_6
+#define Pin6_GPIO_Port GPIOB
+#define ROW0_Pin GPIO_PIN_8
+#define ROW0_GPIO_Port GPIOB
+#define ROW1_Pin GPIO_PIN_9
+#define ROW1_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
